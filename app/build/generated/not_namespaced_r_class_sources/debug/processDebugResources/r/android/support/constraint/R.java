2860718c6a6a301c9378390da64d95a04abb4db6
orignallazycoder <technoarpitorignal@gmail.com>
2019-06-04 09:32:34 +0530

    Fixed Login module - now new UI is being added at new login Added logout button , app icon in middle in HomeMainActivity

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080025;
-        public static final int end = 0x7f08004a;
-        public static final int gone = 0x7f080058;
-        public static final int invisible = 0x7f080063;
-        public static final int left = 0x7f080068;
-        public static final int packed = 0x7f08007f;
-        public static final int parent = 0x7f080081;
-        public static final int percent = 0x7f080084;
-        public static final int right = 0x7f080089;
-        public static final int spread = 0x7f0800ad;
-        public static final int spread_inside = 0x7f0800ae;
-        public static final int start = 0x7f0800b3;
-        public static final int top = 0x7f0800c9;
-        public static final int wrap = 0x7f0800da;
+        public static final int end = 0x7f08004b;
+        public static final int gone = 0x7f080059;
+        public static final int invisible = 0x7f080064;
+        public static final int left = 0x7f080069;
+        public static final int packed = 0x7f080081;
+        public static final int parent = 0x7f080083;
+        public static final int percent = 0x7f080086;
+        public static final int right = 0x7f08008b;
+        public static final int spread = 0x7f0800af;
+        public static final int spread_inside = 0x7f0800b0;
+        public static final int start = 0x7f0800b5;
+        public static final int top = 0x7f0800cb;
+        public static final int wrap = 0x7f0800dc;
     }
     public static final class styleable {
         private styleable() {}
