314a861b12f222f10adf10cea6021ace669ecfa0
orignallazycoder <technoarpitorignal@gmail.com>
2019-06-05 11:04:14 +0530

    New Activity Made - Meditate Module

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/firebase/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/firebase/R.java
@@ -89,28 +89,28 @@ public final class R {
         public static final int actions = 0x7f080018;
         public static final int async = 0x7f080021;
         public static final int blocking = 0x7f080025;
-        public static final int cancel_action = 0x7f08002e;
-        public static final int chronometer = 0x7f080035;
-        public static final int end_padder = 0x7f08004e;
-        public static final int forever = 0x7f08005a;
-        public static final int icon = 0x7f080062;
-        public static final int icon_group = 0x7f080063;
-        public static final int info = 0x7f080067;
-        public static final int italic = 0x7f080069;
-        public static final int line1 = 0x7f08006f;
-        public static final int line3 = 0x7f080070;
-        public static final int media_actions = 0x7f080076;
-        public static final int normal = 0x7f080081;
-        public static final int notification_background = 0x7f080082;
-        public static final int notification_main_column = 0x7f080083;
-        public static final int notification_main_column_container = 0x7f080084;
-        public static final int right_icon = 0x7f080093;
-        public static final int right_side = 0x7f080094;
-        public static final int status_bar_latest_event_content = 0x7f0800be;
-        public static final int text = 0x7f0800c6;
-        public static final int text2 = 0x7f0800c7;
-        public static final int time = 0x7f0800cf;
-        public static final int title = 0x7f0800d0;
+        public static final int cancel_action = 0x7f08002f;
+        public static final int chronometer = 0x7f080036;
+        public static final int end_padder = 0x7f08004f;
+        public static final int forever = 0x7f08005b;
+        public static final int icon = 0x7f080063;
+        public static final int icon_group = 0x7f080064;
+        public static final int info = 0x7f080068;
+        public static final int italic = 0x7f08006a;
+        public static final int line1 = 0x7f080070;
+        public static final int line3 = 0x7f080071;
+        public static final int media_actions = 0x7f080077;
+        public static final int normal = 0x7f080082;
+        public static final int notification_background = 0x7f080083;
+        public static final int notification_main_column = 0x7f080084;
+        public static final int notification_main_column_container = 0x7f080085;
+        public static final int right_icon = 0x7f080094;
+        public static final int right_side = 0x7f080095;
+        public static final int status_bar_latest_event_content = 0x7f0800bf;
+        public static final int text = 0x7f0800c7;
+        public static final int text2 = 0x7f0800c8;
+        public static final int time = 0x7f0800d0;
+        public static final int title = 0x7f0800d1;
     }
     public static final class integer {
         private integer() {}
@@ -122,21 +122,21 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0b0031;
-        public static final int notification_action_tombstone = 0x7f0b0032;
-        public static final int notification_media_action = 0x7f0b0033;
-        public static final int notification_media_cancel_action = 0x7f0b0034;
-        public static final int notification_template_big_media = 0x7f0b0035;
-        public static final int notification_template_big_media_custom = 0x7f0b0036;
-        public static final int notification_template_big_media_narrow = 0x7f0b0037;
-        public static final int notification_template_big_media_narrow_custom = 0x7f0b0038;
-        public static final int notification_template_custom_big = 0x7f0b0039;
-        public static final int notification_template_icon_group = 0x7f0b003a;
-        public static final int notification_template_lines_media = 0x7f0b003b;
-        public static final int notification_template_media = 0x7f0b003c;
-        public static final int notification_template_media_custom = 0x7f0b003d;
-        public static final int notification_template_part_chronometer = 0x7f0b003e;
-        public static final int notification_template_part_time = 0x7f0b003f;
+        public static final int notification_action = 0x7f0b0033;
+        public static final int notification_action_tombstone = 0x7f0b0034;
+        public static final int notification_media_action = 0x7f0b0035;
+        public static final int notification_media_cancel_action = 0x7f0b0036;
+        public static final int notification_template_big_media = 0x7f0b0037;
+        public static final int notification_template_big_media_custom = 0x7f0b0038;
+        public static final int notification_template_big_media_narrow = 0x7f0b0039;
+        public static final int notification_template_big_media_narrow_custom = 0x7f0b003a;
+        public static final int notification_template_custom_big = 0x7f0b003b;
+        public static final int notification_template_icon_group = 0x7f0b003c;
+        public static final int notification_template_lines_media = 0x7f0b003d;
+        public static final int notification_template_media = 0x7f0b003e;
+        public static final int notification_template_media_custom = 0x7f0b003f;
+        public static final int notification_template_part_chronometer = 0x7f0b0040;
+        public static final int notification_template_part_time = 0x7f0b0041;
     }
     public static final class string {
         private string() {}
---
+++ app/src/main/java/com/downbeat/downbeat/Meditate/MeditateActivity.java
@@ -0,0 +1,15 @@
+package com.downbeat.downbeat.Meditate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.downbeat.downbeat.R;
+
+public class MeditateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meditate);
+    }
+}
